d616393292bd22e9496fdad878efed695b6a9e26
to-Reeah <victoriachristopher02@gmail.com>
2022-09-13 14:05:34 -0700

    mikayla

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,12 +7,12 @@ int main(void)
 {
 	_putchar('_');
 	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
 	_putchar('a');
-	_putchar('v');
-	_putchar('w');
-	_putchar('o');
-	_putchar('x');
-	_putchar('q');
+	_putchar('r');
 	_putchar('\n');
 return (0);
 }
