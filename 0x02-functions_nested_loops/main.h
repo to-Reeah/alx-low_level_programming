ebcb40aab64853742accdbcb76cc1a1802bf179d
to-Reeah <victoriachristopher02@gmail.com>
2022-09-13 12:48:02 -0700

    my main

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+1. _putchar
+2.  I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game
+3. 10 x alphabet
+4. islower
+5. is aplha
+6. sign
+7. There is no such thing as absolute value in this world. You can only estimate what a thing is worth to you
+8.  There are only 3 colors, 10 digits, and 7 notes; it's what we do with them that's important
+9. I'm federal agent Jack Bauer, and today is the longest day of my life
+10. learn your times table
+11. a + b
+12. 98 Battery Street, the OG ..
