53fd1b93a599aeac0b753fdc1a74ec8ea593f59e
to-Reeah <victoriachristopher02@gmail.com>
2022-09-14 12:18:44 -0700

    no cap

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,14 +1,23 @@
 #include "main.h"
 /**
- * print_last_digit - prints the last digit of n
- * description: @n int type number
- * Return: return value of last digit of n
+ * print_last_digit - print the last digit of a number
+ * @n: int type number
+ * Return: return value of last digit
  */
 int print_last_digit(int n)
 {
-	n = n % 10;
+	int nc;
+
 	if (n < 0)
-		n = -n;
-	-putchar('0' + (n));
-	return (n);
+	{
+		nc = -1 * (n % 10);
+		_putchar(nc + '0');
+		return (nc);
+	}
+	else
+	{
+		nc = n % 10;
+		_putchar(nc + '0');
+		return (nc);
+	}
 }
