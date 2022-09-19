d98dda7056839e9d5c929415a65141d786886889
to-Reeah <victoriachristopher02@gmail.com>
2022-09-19 05:46:57 -0700

    lets start

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * reset_to_98 - takes a pointer to an int and updates the value
+ * @n: takes an int
+ *
+ * Return: value
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}#include "main.h"
+/**
+ * reset_to_98 - takes a pointer to an int and updates the value
+ * @n: takes an int
+ *
+ * Return: value
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}#include "main.h"
+/**
+ * reset_to_98 - takes a pointer to an int and updates the value
+ * @n: takes an int
+ *
+ * Return: value
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
