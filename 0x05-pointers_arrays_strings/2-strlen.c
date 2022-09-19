8aa834a73e46b664aa56fc2ef6a646301ff19780
to-Reeah <victoriachristopher02@gmail.com>
2022-09-19 07:23:45 -0700

    worker

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - function that returns the length of a string
+ * @s: is a character
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int c;
+
+	for (c = 0; s[c] != '\0'; c++)
+	{
+
+	}
+	return (c);
+}
